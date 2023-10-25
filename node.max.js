1fea5408dbfa3b0efdefaf9078a7b1535286c4d1
sandeepredy6 <145992263+sandeepredy6@users.noreply.github.com>
2023-10-25 11:34:22 +0530

    Add files via upload

---
+++ node.max.js
@@ -0,0 +1,18 @@
+let a=500
+let b=300
+let c=23
+let max,min
+
+if(a>b){
+    max=a
+    min=b
+}
+else{
+    max=b
+    min=a
+}
+if(c>max) max=c
+if(c<min) min=c
+console.log("max---->"+max)
+console.log("min---->"+min)
+ati-cziw-row
