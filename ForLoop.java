33514a70ed9b5b8bdd518b71626561716145620e
sandeepredy6 <145992263+sandeepredy6@users.noreply.github.com>
2023-11-03 13:22:40 +0530

    Add files via upload

---
+++ ForLoop.java
@@ -0,0 +1,15 @@
+package com.uno;
+
+public class ForLoop {
+
+	public static void main(String[] args) {
+		int lastnumber = 10;
+		int sum = 0;
+		
+		
+  for( int startingnumber = 1;startingnumber <=lastnumber; startingnumber++) {
+	  sum = sum + startingnumber;}
+  System.out.println("\n sum =" + sum);
+  }
+	}
+
