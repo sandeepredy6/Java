9de0137713e2ccc1fdce9134285386921924326a
sandeepredy6 <145992263+sandeepredy6@users.noreply.github.com>
2023-11-03 14:15:27 +0530

    Add files via upload

---
+++ ForEachLoop.java
@@ -0,0 +1,18 @@
+package com.uno;
+
+public class ForEachLoop {
+
+	public static void main(String[] args) {
+		
+		for(int i=0;i<10;i++) {
+			for (int j=1;j<=i;j++) {
+			System.out.print("*");
+			}
+		System.out.println();
+		}
+		
+				
+	}
+
+}
+ 
